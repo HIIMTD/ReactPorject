f27200f773bf188b644eecc20c668fc8f2f1ec1d
De Tao <td950614@gmail.com>
2019-04-27 16:04:09 -0400

    *initial setting +reset.css

---
+++ jianshu/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {GlobalStyle} from './style.js'
+
+class App extends Component {
+  render() {
+    return (
+      <fragment>
+      <GlobalStyle />
+      < div className ="dell">
+      
+        hello world
+    </div >
+    
+   </fragment>
+    );
+  }
+}
+
+export default App;
