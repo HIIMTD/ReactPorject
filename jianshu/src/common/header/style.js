fb0301f8e69387d4bebecb7a5a0a924364df3874
De Tao <td950614@gmail.com>
2019-04-27 21:02:34 -0400

    *header/index.js *header/style.js
    hearder part implemented

--- jianshu/src/common/header/style.js
+++ jianshu/src/common/header/style.js
@@ -25,17 +25,75 @@ export const Nav = styled.div`
     width:960px;
     height:100%;
     margin:0 auto ;
-    background:green;
-
+    padding-right:70px;
+    box-sizing:border-box;
 ` ;
 
 export const NavItem = styled.div` 
+
+line-height:56px;
+padding:0 15px;
+font-siez:17px;
+color:#333;
 &.left {
     float:left;
 }
 
 &.right{
-    float right;
+    float: right;
+    color:#969696;
 }
 
+&.active{
+    color:#ea6f5a;
+}
+
+` ;
+
+
+export const NavSearch = styled.input.attrs({
+    placeholder:'search'
+})` 
+    width:160px;
+    height:38px;
+    border:none;
+    outline:none;
+    border-radius:19px;
+    background:#eee;
+    margin-top:9px;
+    margin-left:20px;
+    padding:0 20px;
+    box-sizing:border-box;
+    font-size:14px;
+    &::placeholder{
+        color:#999;
+    }
+` ;
+
+
+export const Addition = styled.div` 
+position:absolute;
+right:0;
+top:0;
+height:56px;
+` ;
+
+
+export const Button = styled.div` 
+float:right;
+line-height:38px;
+border-radius:19px;
+margin-top:9px;
+border:1px solid #ec6149;
+margin-right:20px;
+padding:0 20px;
+font-size:14px;
+&.reg{
+    color:#ec6149;
+}
+
+&.writting{
+    color:#fff;
+    background:#ec6149;
+}
 ` ;
