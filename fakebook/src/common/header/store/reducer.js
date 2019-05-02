60c220251e0ecee0130d7e85a30cd93251fa580f
De Tao <td950614@gmail.com>
2019-05-02 19:04:52 -0400

    use immutable set and get function to prevent origin state change

--- fakebook/src/common/header/store/reducer.js
+++ fakebook/src/common/header/store/reducer.js
@@ -1,20 +1,24 @@
-import * as constants from './constants'
+import * as constants from './constants';
+import {fromJS} from 'immutable';
 
-const defaultState ={
+
+const defaultState = fromJS({
     focused: false
-};
+});
 
 export default(state = defaultState, action)=>{
     if (action.type === constants.SEARCH_FOCUS) {
-        return{
-            focused:true
-        }
+        return state.set('focused',true);
+        // {
+        //     focused:true
+        // }
     }
 
     if (action.type === constants.SEARCH_BLUR) {
-        return{
-            focused:false
-        }
+        return state.set('focused',false);
+        // {
+        //     focused:false
+        // }
     }
     return state;
 }
