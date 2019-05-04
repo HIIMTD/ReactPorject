66971bc0893442df1125d8d55805aa233cf5e920
De Tao <td950614@gmail.com>
2019-05-04 17:29:55 -0400

    initial four component on the home page

---
+++ fakebook/src/pages/home/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { HomeWrapper, HomeLeft, HomeRight } from './style';
+import Topic from './component/Topics'
+import Writer from './component/Writer'
+import Recommend from './component/Recommend'
+import List from './component/List'
+
+class Home extends Component {
+    render() {
+        return (
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className='banner-image' src={require('../../statics/Homeleft.png')} alt="" />
+                    <Topic />
+                    <List />
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend />
+                    <Writer />
+                </HomeRight>
+            </HomeWrapper>
+        )
+    }
+}
+
+export default Home;
---
+++ fakebook/src/pages/home/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const HomeWrapper = styled.div`
+    width:960px;
+    margin:0 auto;
+    overflow:hidden;
+    background:red;
+`;
+
+export const HomeLeft = styled.div`
+    width:625px;
+    margin-left:15px;
+    padding-top:30px;
+    float:left;
+    .banner-image{
+        width:625px;
+        height:240px;
+    }
+
+`;
+
+
+export const HomeRight = styled.div`
+    width:240px;
+    float:right;
+
+`;
