bb501c3c96d55f537cb0d407267cd2b6a646bae3
De Tao <td950614@gmail.com>
2019-05-06 00:27:40 -0400

    +simple detail page for router test

--- fakebook/src/pages/detail/index.js
+++ fakebook/src/pages/detail/index.js
@@ -1,9 +1,18 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { DetailWrapper, Header, Content } from './style';
 
-class Detail extends Component{
-    render(){
+class Detail extends Component {
+    render() {
         return (
-            <div>detail</div>
+            <DetailWrapper>
+                <Header> Newest trailer:Ironman dies in averger 4</Header>
+                <Content>
+                    <img src={require('../../statics/ironman2.jpg')} alt='' />
+                    <p>Yes TONY STARK(iron man)DIES!! in Avengers :ENDGAME</p>
+                    <p>Avengers endgame is actually a roller coaster ride for everyone from time travel (Time heist) to the death of our favourite characters.</p>
+                    <p>Endgame starts off with Tony and nebula floating in the space, then captain marvel finding them and bringing them back to earth. Tony meets cap and he was angry as they lost. Captain marvel and other decide to go and get thanos but as tony was not in the condition he couldn’t join them. They find thanos on the planet alone and then kill him off(Thor goes for the head).</p>
+                </Content>
+            </DetailWrapper>
         )
     }
 }
